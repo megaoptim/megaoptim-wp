4d8c22dbb8dd93101369d997f527434c2ce79edb
gdarko <dg@darkog.com>
2018-10-06 19:15:46 +0000

    Correct wrong tokens number in the welcome message

--- includes/views/settings/key.php
+++ includes/views/settings/key.php
@@ -1,6 +1,6 @@
 <div id="megaoptim-apikey" class="text-center">
     <h1><?php _e( 'Getting Started', 'megaoptim' ); ?></h1>
-    <p><?php _e( 'It seems that you don\'t have API key set up. In order to use MegaOptim you need to obtain free API key which will give you 150 credits per month for free.', 'megaoptim' ); ?></p>
+    <p><?php _e( 'It seems that you don\'t have API key set up. In order to use MegaOptim you need to obtain free API key which will give you 200 tokens per month for free.', 'megaoptim' ); ?></p>
     <p><?php echo sprintf( __( 'If you aren\'t registered yet, click %s to register and get api key.', 'megaoptim' ), '<a href="'.WP_MEGAOPTIM_REGISTER_URL.'">' . __( 'here', 'megaoptim' ) . '</a>' ); ?></p>
     <div>
         <div class="megaoptim-row">
