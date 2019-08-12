71183a81ff2b80d3398d932e9733274b390172fe
Darko Gjorgjijoski <dg@darkog.com>
2019-08-12 21:30:57 +0000

    Notices & Welcome screen improvements

--- includes/hooks/notices.php
+++ includes/hooks/notices.php
@@ -4,55 +4,58 @@
  * Notify the user about zero user balance
  */
 function _megaoptim_notice_user_balance() {
-	if(!is_admin()) {
+	if ( ! is_admin() ) {
 		return;
 	}
 	try {
-		$profile = new MGO_Profile();
+		$profile      = new MGO_Profile();
+		$api_key      = MGO_Settings::instance()->get( MGO_Settings::API_KEY );
 		$tokens_count = $profile->get_tokens_count();
-		if( intval($tokens_count) === 0) {
+		if ( !empty($api_key) && intval( $tokens_count ) === 0 ) {
 			$message = sprintf(
 				'%s %s %s %s.',
-				__('Your image tokens balance is 0, to continue using', 'megaoptim'),
-				'<strong>'.__('MegaOptim Image Optimizer', 'megaoptim').'</strong>',
-				__('please top up your account'),
-				'<a href="'.WP_MEGAOPTIM_DASHBOARD_URL.'">'.__('here', 'megaoptim').'</a>'
+				__( 'Your image tokens balance is 0. To continue using', 'megaoptim' ),
+				'<strong>' . __( 'MegaOptim Image Optimizer', 'megaoptim' ) . '</strong>',
+				__( 'please top up your account' ),
+				'<a href="' . WP_MEGAOPTIM_DASHBOARD_URL . '">' . __( 'here', 'megaoptim' ) . '</a>'
 			);
-			MGO_Admin_Notices::instance()->warning('insufficient_balance', $message, 1);
+			MGO_Admin_Notices::instance()->warning( 'insufficient_balance', $message, 1 );
 		}
-	} catch (\Exception $exception) {
+	} catch ( \Exception $exception ) {
 
 	}
 }
-add_action('init', '_megaoptim_notice_user_balance', 1000);
+
+add_action( 'init', '_megaoptim_notice_user_balance', 1000 );
 
 /**
  * Notify the user about conflicting plugins
  * // TODO: Deactivate button.
  */
 function _megaoptim_notice_conflicting_plugins() {
-	if(!is_admin()) {
+	if ( ! is_admin() ) {
 		return;
 	}
 
 	$active_plugins = megaoptim_get_conflicting_plugins();
-	if(is_array($active_plugins) && count($active_plugins)) {
+	if ( is_array( $active_plugins ) && count( $active_plugins ) ) {
 		$message = sprintf(
 			'%s %s:',
-			__('The following plugins will likely conflict with', 'megaoptim'),
-			'<strong>'.__('MegaOptim Image Optimizer') . '</strong>'
+			__( 'The following plugins will likely conflict with', 'megaoptim' ),
+			'<strong>' . __( 'MegaOptim Image Optimizer' ) . '</strong>'
 		);
-		$message = '<p>'.$message.'</p>';
+		$message = '<p>' . $message . '</p>';
 		$message .= '<ul>';
-		foreach ($active_plugins as $name => $data) {
-			$message .= '<li>'.$name.'</li>';
+		foreach ( $active_plugins as $name => $data ) {
+			$message .= '<li>' . $name . '</li>';
 		}
 		$message .= '</ul>';
-		$message .= __('Please consider deactivating those plugins to ensure better integration with MegaOptim. Only leave those plugins active if you know what you are doing.', 'megaoptim');
+		$message .= __( 'Please consider deactivating those plugins to ensure better integration with MegaOptim. Only leave those plugins active if you know what you are doing.', 'megaoptim' );
 
-		MGO_Admin_Notices::instance()->warning('conflicting_plugins', $message, 1);
+		MGO_Admin_Notices::instance()->warning( 'conflicting_plugins', $message, 1 );
 	}
 
 
 }
-add_action('init', '_megaoptim_notice_conflicting_plugins', 1000);
+
+add_action( 'init', '_megaoptim_notice_conflicting_plugins', 1000 );
--- includes/views/misc/instructions.php
+++ includes/views/misc/instructions.php
@@ -2,8 +2,9 @@
     <div class="megaoptim-card megaoptim-card-shadow">
         <div class="megaoptim-row megaoptim-header">
             <div class="megaoptim-colf">
-                <p class="lead"><?Php _e( 'Thanks for installing', 'megaoptim' ); ?> <strong class="green"><?php _e( 'MegaOptim Image Optimizer', 'megaoptim' ); ?></strong></p>
-                <p class="desc"><?php echo sprintf(__('To %s with image optimization follow the steps below.', 'megaoptim'), '<strong>'.__('get started', 'megaoptim').'</strong>'); ?></p>
+                <p class="lead"><?Php _e( 'Thanks for installing', 'megaoptim' ); ?> <strong
+                            class="green"><?php _e( 'MegaOptim Image Optimizer', 'megaoptim' ); ?></strong></p>
+                <p class="desc"><?php echo sprintf( __( 'To %s with image optimization follow the steps below.', 'megaoptim' ), '<strong>' . __( 'get started', 'megaoptim' ) . '</strong>' ); ?></p>
             </div>
         </div>
         <div class="megaoptim-row">
@@ -11,27 +12,31 @@
                 <div class="megaoptim-instruction">
                     <h4 class="navy"><?php _e( '1. Obtain API Key', 'megaoptim' ); ?></h4>
                     <p>
-						<?php echo sprintf( __( 'The API key is essential to use the plugin, you can get it by clicking the button below. The initial FREE plan starts from %s tokens a month. You can also purcahse larger quota anytime from our %s.', 'megaoptim' ), '<strong>500</strong>', '<a title="'.__('In the dahsboard you can monitor see your optimization reports, api key, open support tickets and much more', 'megaoptim').'" href="https://megaoptim.com/dashboard/api/credentials" target="_blank">' . __( 'dashboard', 'megaoptim' ) . '</a>' ); ?>
+						<?php echo sprintf( __( 'The API key is essential to use the plugin, you can get it by clicking the button below. The initial FREE plan starts from %s tokens a month. You can also purcahse larger quota anytime from our %s.', 'megaoptim' ), '<strong>500</strong>', '<a title="' . __( 'In the dahsboard you can monitor see your optimization reports, api key, open support tickets and much more', 'megaoptim' ) . '" href="https://megaoptim.com/dashboard/api/credentials" target="_blank">' . __( 'dashboard', 'megaoptim' ) . '</a>' ); ?>
                     </p>
-                    <p><a target="_blank" id="megaoptim-trigger-register" data-remodal-target="megaoptim-register" class="button button-primary"><?php _e('Get API Key','megaoptim'); ?></a> or <a href="<?php echo WP_MEGAOPTIM_REGISTER_URL; ?>" target="_blank">register here</a> </p>
+                    <p><a target="_blank" id="megaoptim-trigger-register" data-remodal-target="megaoptim-register"
+                          class="button button-primary"><?php _e( 'Get API Key', 'megaoptim' ); ?></a> or <a
+                                href="<?php echo WP_MEGAOPTIM_REGISTER_URL; ?>" target="_blank">register here</a></p>
                 </div>
             </div>
             <div class="megaoptim-col3">
                 <div class="megaoptim-instruction">
-                    <h4 class="navy"><?php _e( '2. Setup your preferences', 'megaoptim' ); ?></h4>
+                    <h4 class="navy"><?php _e( '2. Enter your API Key', 'megaoptim' ); ?></h4>
                     <p>
-                       <?php echo sprintf(__('On the "Settings" page you need to %s from step 1 and can configure how the plugin behaves. Various options are available like auto-optimization on upload, which image sizes to be optimized, backup settings, etc.', 'megaoptim'), '<strong>'.__('enter the api key','megaoptim').'</strong>'); ?>
+						<?php echo sprintf( __( 'On the "Settings" page you need to %s from step 1 and you can configure how the plugin behaves. Various options are available like auto-optimization on upload, which image sizes to be optimized, backup settings, etc.', 'megaoptim' ), '<strong><u>' . __( 'enter the api key', 'megaoptim' ) . '</u></strong>' ); ?>
                     </p>
-                    <p><a href="<?php echo MGO_Admin_UI::get_settings_url(); ?>" class="button button-primary"><?php _e('Go to Settings','megaoptim'); ?></a></p>
+                    <p><a href="<?php echo MGO_Admin_UI::get_settings_url(); ?>"
+                          class="button button-primary"><?php _e( 'Go to Settings', 'megaoptim' ); ?></a></p>
                 </div>
             </div>
             <div class="megaoptim-col3">
                 <div class="megaoptim-instruction">
                     <h4 class="navy"><?php _e( '3. Start Optimizing', 'megaoptim' ); ?></h4>
                     <p>
-                        <?php _e('You can optimize your Media Library, NextGen Galleries or Local Folders using the bulk optimizer, optimize them one by one from the media screens or turn on auto optimiza on upload to optimize the images immediately when they are uploaded.', 'megaoptim'); ?>
+						<?php echo sprintf( __( 'You can optimize your %s . The plugin supports bulk mode, one by one from the Media page in the admin or auto optimization after the upload. Auto optimization is turned on by default.', 'megaoptim' ), '<strong>' . __( 'Media Library, NextGen Galleries or Local Folders', 'megaoptim' ) . '</strong>' ); ?>
                     </p>
-                    <p><a href="<?php echo MGO_Admin_UI::get_optimizer_url(); ?>" class="button button-primary"><?php _e('Start Optimizing','megaoptim'); ?></a></p>
+                    <p><a href="<?php echo MGO_Admin_UI::get_optimizer_url(); ?>"
+                          class="button button-primary"><?php _e( 'Start Optimizing', 'megaoptim' ); ?></a></p>
                 </div>
             </div>
         </div>
@@ -41,11 +46,12 @@
         <div class="megaoptim-row">
             <div class="megaoptim-colf">
                 <div class="megaoptim-extra">
-                    <h4 class="navy"><?php _e('Referral program', 'megaoptim'); ?></h4>
-                    <p><?php echo sprintf(__('We have a referral program available for everyone. Help us spread by sharing your %s with your friends and get %s on each signup and %s when the referral subscribes to any plan.'), '<strong><a target="_blank" href="https://megaoptim.com/dashboard/referral">'.__('referral url').'</a></strong>', '<strong>'.__('120 tokens', 'megaoptim').'</strong>', '<strong>'.__('500 tokens', 'megaoptim').'</strong>');?></p>
+                    <h4 class="navy"><?php _e( 'Referral program', 'megaoptim' ); ?></h4>
+                    <p><?php echo sprintf( __( 'We have a referral program available for everyone. Help us spread by sharing your %s with your friends and get %s on each signup and %s when the referral subscribes to any plan.' ), '<strong><a target="_blank" href="https://megaoptim.com/dashboard/referral">' . __( 'referral url' ) . '</a></strong>', '<strong>' . __( '120 tokens', 'megaoptim' ) . '</strong>', '<strong>' . __( '500 tokens', 'megaoptim' ) . '</strong>' ); ?></p>
                 </div>
             </div>
         </div>
-        <button type="button" class="notice-dismiss dismiss-megaoptim-notice"><span class="screen-reader-text"><?php _e('Dismiss this notice', 'megaoptim'); ?>.</span></button>
+        <button type="button" class="notice-dismiss dismiss-megaoptim-notice"><span
+                    class="screen-reader-text"><?php _e( 'Dismiss this notice', 'megaoptim' ); ?>.</span></button>
     </div>
 </div>
